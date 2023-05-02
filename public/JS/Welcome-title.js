53d4e5be456cbe2ca705517d6c2d22c823f8f6c1
fromOmer <128833806+fromOmer@users.noreply.github.com>
2023-05-02 15:28:54 +0300

    added files

---
+++ public/JS/Welcome-title.js
@@ -0,0 +1,17 @@
+function welcomingTitle() {
+    console.log("Welcome");
+    var date = new Date();
+    var hours = date.getUTCHours();
+    if (5 < hours && hours < 11) {
+        document.querySelector('.topBar').innerHTML = "Welcome, Good Morning";
+    }
+    else if (11 < hours && hours < 18) {
+        document.querySelector('.topBar').innerHTML = "Welcome, Good Afternoon";
+    }
+    else if (18 < hours && hours < 21) {
+        document.querySelector('.topBar').innerHTML = "Welcome, Good Evening";
+    }
+    else {
+        document.querySelector('.topBar').innerHTML = "Welcome, Good Night";
+    }
+}

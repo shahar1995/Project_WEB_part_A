15b2b44a12a502f2d9ab1bbf48b0e1b53d0ef9d9
shahar bar <127855525+shahar1995@users.noreply.github.com>
2023-05-03 20:52:32 +0300

    new

---
+++ public/backup_old/new.js
@@ -0,0 +1,39 @@
+
+const form_sign = document.querySelector('.form-container');
+    const inputID = document.querySelector("#ID");
+ const emailInput = document.querySelector('#Email');
+    form_sign.addEventListener("submit",function(event) {
+        // event.preventDefault();
+
+
+        form.addEventListener('submit', function (event) {
+            // Prevent the form from submitting if validation fails
+
+            if ( validateEmail() && validateID()) {
+                alert("You successfully registered to the class");
+            }
+            else
+            {
+                event.preventDefault();
+            }
+        })
+    });
+    function validateID() {
+ const ID = inputID.value;
+const regex = /\b\d{8}\b/;
+if (!regex.test(ID)) {
+    alert("ID must be 8 digits.");
+    return false;
+  }
+  return true;
+}
+function validateEmail() {
+  const emailValue = emailInput.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(emailValue)) {
+    alert('Please enter a valid email address.');
+    emailInput.focus();
+    return false;
+  }
+  return true;
+}

0c95413628493c6a349a4ad1c24e10f9d027997d
shahar <sbaruc@post.bgu.ac.il>
2023-05-03 19:05:16 +0300

    new

---
+++ public/JS/Sign in.js
@@ -0,0 +1,38 @@
+//Take a values from information json for sign in //
+
+    const form_sign = document.querySelector('.sign-in-form');
+    const customerID = document.querySelector("#ID");
+    const customerPassword = document.querySelector("#password");
+    form_sign.addEventListener("submit",function(event) {
+    event.preventDefault();
+
+    const ID = customerID.value;
+    const password= customerPassword.value;
+    Valid_user(ID, password)
+        .then(result => {
+            if (result) {
+
+                Go_to_Home_Page();
+            } else {
+                alert("Invalid details, Please check again your ID and password");
+            }
+        })
+            .catch(error => {
+            // alert("Please try again later");
+            console.error(error);
+        });
+    });
+
+    function Go_to_Home_Page() {
+    window.location.href = "Review.html";
+}
+
+async function Valid_user(ID, password) {
+    const response = await fetch("../public/JSON/trainers.json");
+    console.log(response)
+    const data =await response.json();
+    const found = data.trainers.find(trainer => trainer.ID === ID && trainer.password === password);
+    console.log('Customer found:', found);
+    return found !== undefined;
+
+}
--- public/JS/Sign up.js
+++ public/JS/Sign up.js
@@ -1,41 +1,5 @@
-//Take a values from information json for sign in //
 
-const form_sign = document.querySelector('.sign-in-form');
-    const customerID = document.querySelector("#ID");
-    const customerPassword = document.querySelector("#password");
-    form_sign.addEventListener("submit",function(event) {
-    event.preventDefault();
 
-    const ID = customerID.value;
-    const password= customerPassword.value;
-    Valid_user(ID, password)
-        .then(result => {
-            if (result) {
-
-                Go_to_Home_Page();
-            } else {
-                alert("Invalid details, Please check again your ID and password");
-            }
-        })
-            .catch(error => {
-            // alert("Please try again later");
-            console.error(error);
-        });
-    });
-
-    function Go_to_Home_Page() {
-    window.location.href = "Review.html";
-}
-
-async function Valid_user(ID, password) {
-    const response = await fetch("../public/JSON/trainers.json");
-    console.log(response)
-    const data =await response.json();
-    const found = data.trainers.find(trainer => trainer.ID === ID && trainer.password === password);
-    console.log('Customer found:', found);
-    return found !== undefined;
-
-}
 
 //invalid values in the sign_up //
 
@@ -87,6 +51,16 @@ function validateEmail() {
   if (!emailRegex.test(emailValue)) {
     alert('Please enter a valid email address.');
     emailInput.focus();
+    return false;
+  }
+  return true;
+}
+
+function validateID() {
+  const id = document.getElementById("ID").value;
+  const regex = /^\d{8}$/;
+  if (!regex.test(id)) {
+    alert("ID must be 8 digits.");
     return false;
   }
   return true;
